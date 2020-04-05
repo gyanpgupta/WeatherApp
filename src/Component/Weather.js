7a34aa6985b68b5811be586f7e317e60c4d2b195
guptagyan58@gmail.com <Gyan>
2020-04-05 21:45:29 +0530

    updated changes

--- src/Component/Weather.js
+++ src/Component/Weather.js
@@ -11,8 +11,8 @@ class DisplayWeather extends React.Component {
     }
   }
 
-  handleClick = (identifyClick) => {
-    if (identifyClick == "next") {
+  handleClick = identifyClick => {
+    if (identifyClick === "next") {
       const count = this.state.page + 21;
       this.setState({ page: count });
     } else {
@@ -25,7 +25,7 @@ class DisplayWeather extends React.Component {
 
   fetchData = () => {
     const paginatedCities = cities.slice(this.state.page, this.state.page + 21);
-    paginatedCities.map(obj => {
+    paginatedCities.forEach(obj => {
       fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${obj.lat}&lon=${obj.lng}&appid=3db51c79aacf7906d01567db5ae4ecfa`)
         .then(res => res.json())
         .then(data => {
@@ -62,25 +62,29 @@ class DisplayWeather extends React.Component {
       <div>
         <h1 className="header">Weather App</h1>
         <table className="table">
-          <tr className="tableRow">
-            <th className="tableHeader">City Name</th>
-            <th className="tableHeader">Latitude</th>
-            <th className="tableHeader">Longitude</th>
-            <th className="tableHeader">Weather Description</th>
-          </tr>
-          {this.state.arr && this.state.arr.map(weatherData => {
-            return (
-              <tr className="tableRow">
-                <td className="tableHeader">{weatherData.cityName}</td>
-                <td className="tableHeader">{weatherData.lat}</td>
-                <td className="tableHeader">{weatherData.lng}</td>
-                <td className="tableHeader">{weatherData.weather}</td>
-              </tr>
-            )
-          })}
-          <button className="previousButton" onClick={() => this.handleClick('previous')}>Prev</button>
-          <button className="nextButton" onClick={() => this.handleClick('next')}>Next</button>
+          <thead>
+            <tr className="tableRow">
+              <th className="tableHeader">City Name</th>
+              <th className="tableHeader">Latitude</th>
+              <th className="tableHeader">Longitude</th>
+              <th className="tableHeader">Weather Description</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.arr && this.state.arr.map((weatherData,index) => {
+              return (
+                <tr className="tableRow" key={index}>
+                  <td className="tableHeader">{weatherData.cityName}</td>
+                  <td className="tableHeader">{weatherData.lat}</td>
+                  <td className="tableHeader">{weatherData.lng}</td>
+                  <td className="tableHeader">{weatherData.weather}</td>
+                </tr>
+              )
+            })}
+          </tbody>
         </table>
+        <button className="previousButton" onClick={() => this.handleClick('previous')}>Prev</button>
+        <button className="nextButton" onClick={() => this.handleClick('next')}>Next</button>
       </div>
     )
   }
